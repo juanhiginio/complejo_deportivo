c3a1e6c8b5539c6424406bd37709bb4e48360450
juanhiginio <juanhiginio06@gmail.com>
2026-04-05 14:35:50 -0500

    Crear proyecto completo

---
+++ src/main/java/com/moviles/complejo_deportivo/ComplejoDeportivoApplication.java
@@ -0,0 +1,13 @@
+package com.moviles.complejo_deportivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplejoDeportivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplejoDeportivoApplication.class, args);
+	}
+
+}
